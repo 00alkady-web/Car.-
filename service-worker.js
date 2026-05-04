955f8e308365fc357177fb8ed53e5a85dfe67db8
00alkady-web <00alkady@gmail.com>
2026-05-04 17:07:11 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'taiz-traffic-cache-v2';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.jsdelivr.net/npm/tesseract.js@4/dist/tesseract.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.bundle.min.js/0.10.1/html2pdf.bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
